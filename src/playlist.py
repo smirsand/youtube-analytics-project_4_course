8b740fe1475d440e8b540a7c9b464b86c5aba225
Sergey <smirsand@mail.ru>
2023-06-27 16:26:55 +0300

    add code to playlist.py

--- src/playlist.py
+++ src/playlist.py
@@ -1,19 +1,22 @@
 from datetime import timedelta
 
 import isodate
-from googleapiclient.errors import HttpError
 
 from src.video import PLVideo, Video, youtube
 
 
 class MixinPlayList:
+    """
+    Mixin для плейлиста.
+    """
     def __init__(self, playlist_id: str):
         self.playlist_id = playlist_id
-        self.title = None
-        self.url = None
 
 
 class PlayList(MixinPlayList, PLVideo, Video):
+    """
+    Класс плейлиста.
+    """
     def __init__(self, playlist_id: str):
         super().__init__(playlist_id)
         self.parts = "snippet,contentDetails,statistics"
@@ -24,63 +27,33 @@ class PlayList(MixinPlayList, PLVideo, Video):
 
     @property
     def total_duration(self) -> timedelta:
-        total = timedelta()
-        try:
-            playlist_response = youtube.playlists().list(part='snippet,contentDetails', id=self.playlist_id).execute()
-            playlist_items = playlist_response['items'][0]['contentDetails']['itemCount']
-            if playlist_response['items']:
-                self.title = playlist_response['items'][0]['snippet']['title']
-                self.url = f"https://www.youtube.com/playlist?list={self.playlist_id}"
-                print(self.title)
-                print(self.url)
-
-            next_page_token = None
-            while True:
-                playlist_items_response = youtube.playlistItems().list(part='contentDetails',
-                                                                       playlistId=self.playlist_id, maxResults=50,
-                                                                       pageToken=next_page_token).execute()
-
-                start_times = []  # перезаписывается на каждой итерации, содержит продолжительности видео
-                for item in playlist_items_response['items']:
-                    video_id = item['contentDetails']['videoId']
-                    video_duration = youtube.videos().list(part='contentDetails', id=video_id).execute()
-                    duration = video_duration['items'][0]['contentDetails']['duration']
-                    start_times.append(duration)
-                    print(start_times)
-
-                total = timedelta()
-                for time in start_times:
-                    total += isodate.parse_duration(time)
-
-                print(str(total))
-                next_page_token = playlist_items_response.get('nextPageToken')
-                if next_page_token is None:
-                    break
-
-            return total
-
-        except HttpError as error:
-            print(f'An error occurred: {error}')
+        playlist_response = youtube.playlists().list(part='snippet,contentDetails', id=self.playlist_id).execute()
+        if playlist_response['items']:
+            self.title = playlist_response['items'][0]['snippet']['title']
+            self.url = f"https://www.youtube.com/playlist?list={self.playlist_id}"
+        while True:
+            playlist_items_response = youtube.playlistItems().list(part='contentDetails',
+                                                                   playlistId=self.playlist_id).execute()
+            start_times = []
+            for item in playlist_items_response['items']:
+                video_id = item['contentDetails']['videoId']
+                video_duration = youtube.videos().list(part='contentDetails', id=video_id).execute()
+                duration = video_duration['items'][0]['contentDetails']['duration']
+                start_times.append(duration)
+            total = timedelta()
+            for time in start_times:
+                total += isodate.parse_duration(time)
             return total
 
     def show_best_video(self) -> str:
-        try:
-            playlist_items = youtube.playlistItems().list(part="snippet", playlistId=self.playlist_id,
-                                                          maxResults=50).execute()
-            best_video = playlist_items['items'][0]['snippet']['resourceId']['videoId']
-            best_likes = 0
+        best_likes = -1
+        playlist_items = youtube.playlistItems().list(part='snippet,contentDetails',
+                                                      playlistId=self.playlist_id, ).execute()
+        for item in playlist_items['items']:
+            video_id = item['snippet']['resourceId']['videoId']
+            video_response = youtube.videos().list(part='statistics', id=video_id).execute()
+            video_likes = int(video_response['items'][0]['statistics']['likeCount'])
+            if video_likes > best_likes:
+                best_video_id = video_id
 
-            for item in playlist_items['items']:
-                video_id = item["snippet"]["resourceId"]["videoId"]
-                rating_response = youtube.videos().getRating(id=video_id).execute()
-                likes = rating_response['items'][0]['rating'].get('likeCount', 0)
-
-                if likes > best_likes:
-                    best_likes = likes
-                    best_video = f"https://www.youtube.com/watch?v={video_id}"
-
-            return best_video
-
-        except HttpError as error:
-            print(f'An error occurred: {error}')
-            return "Video not found"
+        return f"https://youtu.be/{best_video_id}"
